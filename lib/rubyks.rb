0f54cdeb06f51cf72e3b05549d3aad402de82157
urthbound <jeffowler@gmail.com>
2013-08-23 18:22:59 -0400

    Adding license

--- lib/rubyks.rb
+++ lib/rubyks.rb
@@ -836,3 +836,45 @@ class Cube
   end
   self
 end
+
+#This is a software model of a Rubik's cube, providing a dynamic data structure with which to describe the state and orientation of any cube in any legal position. 
+#
+#I have included a set of solver methods that work every time, although pretty inefficiently. If you come across an edge case, please let me know!
+#
+#It's my hope that some people will use this nomenclature to write more efficient solving algorithms than mine. My intent was to reproduce my own thought process when solving, and I am not a speedcuber, I just know one basic way to solve the cube and wanted to see if I could make my program run that same routine (more or less).  
+#
+#
+#DATA STRUCTURE:
+#
+#The @cube attribute contains 6 arrays of 9 elements, each array describes one side of the cube like so:
+#
+#    @cube[0] - top
+#    @cube[1] - left
+#    @cube[2] - back
+#    @cube[3] - right
+#    @cube[4] - front
+#    @cube[5] - bottom
+#
+#The first element @cube[x][0] in any array describes the center square of that face, and remains static in relation to the other sides, just as a real cube's center square would. 
+#
+#The remaining array elements from [1] - [8] start at "12 o'clock" and move clockwise. Thus: Even numbers are middle cubies and odd numbers are always corner cubies.
+#      
+#      |8|1|2|
+#      |7|0|3|
+#      |6|5|4|
+#
+#"12 o'clock" is constant amongst all sides of the cube, and refers to what "north" would be if the cube was unfolded into two dimensions like so:
+#
+#          1
+#      5,4,0,2
+#          3
+#
+#When cube orientation procedures are applied, the "address" of each side remains consistent with the above diagram, even as the sides themselves represent different numbers. (The sides could just as easily be assigned any symbol or string or whatnott, just as long as each side started off all the same thing, the cube would be in a legal state).
+#
+#For example: if you created a new cube and then applied Cube#turn, the sides would appear like so:
+#
+#          4
+#      5,3,0,1
+#          2
+#
+#But the address of the location of these sides would remain static. This is so that complex move combinations can be applied to a cube regardless of its orientation to the "viewer."
--- rubyks.gemspec
+++ rubyks.gemspec
@@ -1,9 +1,10 @@
 Gem::Specification.new do |s|
   s.name        = "rubyks"
-  s.version     = "0.0.4"
+  s.license     = "MIT"
+  s.version     = "0.0.5"
   s.summary     = "Rubik's cube model and simple solving algorithm"
   s.date        = "2013-08-22"
-  s.description = "Provides a data structure to represent a Rubik's cube and functionality to perform simple transformations on that cube, in addition to a simple solving algorithm."
+  s.description = "Provides a data structure to represent a Rubik's cube and functionality to perform simple transformations on that cube, in addition to a basic solving algorithm."
   s.authors     = ["Jeff Fowler"]
   s.email       = ["jeff@jeffalanfowler.com"]
   s.homepage    = "http://github.com/urthbound/rubyks"
