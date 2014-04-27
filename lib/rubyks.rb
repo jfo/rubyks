a2934ba43493a4a3e332dca87da14e565a781380
Will Sommers <will.sommers@gmail.com>
2014-04-27 14:41:59 -0400

    Print using methods rather than long strings.

--- lib/rubyks.rb
+++ lib/rubyks.rb
@@ -805,30 +805,41 @@ class Cube
     end
   end
 
+  def print_square(side)
+    rows = [[8, 1, 2], [7, 0, 3], [6, 5, 4]]
+     
+    rows.map do |row|
+      row.map { |pos| "|#{@cube[side][pos]}|" }.join
+    end
+  end
+
+  def print_side(side)
+    line_break = "---------"
+    line_array = (0..3).map { |x| line_break } 
+
+    side_array = print_square(side)
+    
+    side_array = [line_break] +  side_array.zip(line_array).flatten
+    
+    if [1, 3].include?(side)
+      spaces = (0...20).map { |x| " "}.join
+      side_array = side_array.map {|item| spaces + item}
+    end
+
+    side_array
+  end
+
   #Outputs an ascii representation of the cube arrays
   def print
-    puts "                    ---------"
-    puts "                    |#{@cube[1][8]}||#{@cube[1][1]}||#{@cube[1][2]}|"
-    puts "                    ---------"
-    puts "                    |#{@cube[1][7]}||#{@cube[1][0]}||#{@cube[1][3]}|"
-    puts "                    ---------"
-    puts "                    |#{@cube[1][6]}||#{@cube[1][5]}||#{@cube[1][4]}|"
-    puts "                    ---------"
-    puts "--------- --------- --------- ---------"
-    puts "|#{@cube[5][8]}||#{@cube[5][1]}||#{@cube[5][2]}| |#{@cube[4][8]}||#{@cube[4][1]}||#{@cube[4][2]}| |#{@cube[0][8]}||#{@cube[0][1]}||#{@cube[0][2]}| |#{@cube[2][8]}||#{@cube[2][1]}||#{@cube[2][2]}|"
-    puts "--------- --------- --------- ---------"
-    puts "|#{@cube[5][7]}||#{@cube[5][0]}||#{@cube[5][3]}| |#{@cube[4][7]}||#{@cube[4][0]}||#{@cube[4][3]}| |#{@cube[0][7]}||#{@cube[0][0]}||#{@cube[0][3]}| |#{@cube[2][7]}||#{@cube[2][0]}||#{@cube[2][3]}|"
-    puts "--------- --------- --------- ---------"
-    puts "|#{@cube[5][6]}||#{@cube[5][5]}||#{@cube[5][4]}| |#{@cube[4][6]}||#{@cube[4][5]}||#{@cube[4][4]}| |#{@cube[0][6]}||#{@cube[0][5]}||#{@cube[0][4]}| |#{@cube[2][6]}||#{@cube[2][5]}||#{@cube[2][4]}|"
-    puts "--------- --------- --------- ---------"
-    puts "                    ---------"
-    puts "                    |#{@cube[3][8]}||#{@cube[3][1]}||#{@cube[3][2]}|"
-    puts "                    ---------"
-    puts "                    |#{@cube[3][7]}||#{@cube[3][0]}||#{@cube[3][3]}|"
-    puts "                    ---------"
-    puts "                    |#{@cube[3][6]}||#{@cube[3][5]}||#{@cube[3][4]}|"
-    puts "                    ---------"
+    sides = []
+    sides << print_side(1)
+    sides << print_side(5).zip(print_side(4), print_side(0), print_side(2))
+                          .map { |row| row.join(" ") } 
+    sides << print_side(3)
+    sides.each { |row| puts row }
+    nil
   end
+
   self
 end
 
